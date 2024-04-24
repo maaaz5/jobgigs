c9ace7feca42e33609c3b2690c21655ed5658124
maaaz5 <mohamed.amine12345@gmail.com>
2024-04-24 01:15:13 +0100

    init

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+
+//All Listings
+Route::get('/', function () {
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => Listing::all()
+    ]);
+});
+
+// Single Listing
+Route::get('/listings/{id}', function ($id) {
+    return view('listing', [
+        'listing' => Listing::find($id)
+    ]);
+});
