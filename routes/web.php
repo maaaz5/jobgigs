ea0ae05124eff366f7ddc0e5428f44d00c6ffe2c
maaaz5 <mohamed.amine12345@gmail.com>
2024-04-25 03:12:40 +0100

    Register user done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 
 //All Listings
@@ -27,8 +28,19 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 // Show Edit Form
 Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 
-
-
-
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+
+//Show Register/Create Form
+Route::get('/register', [UserController::class, 'create']);
+
+
+//Create new user
+Route::post('/users', [UserController::class, 'store']);
+
+//Log user out
+Route::post('/logout', [UserController::class, 'logout']);
+
+//show login form
+Route::get('/login', [UserController::class, 'login']);
