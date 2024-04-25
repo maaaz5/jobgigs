a7e3a57e303c78c3be058dbcf2028188f1ceafeb
maaaz5 <mohamed.amine12345@gmail.com>
2024-04-25 02:42:18 +0100

    Listing crud

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+
 
 //All Listings
 Route::get('/', [ListingController::class, 'index']);
@@ -16,6 +16,18 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 
 // Store Listing Data
 Route::post('/listings', [ListingController::class, 'store']);
+
+
+// Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+// Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+
 
 
 // Single Listing
